4effe5816b38837193c527a2a04378d59153c680
Paz Ortega <paz.ortega.contreras@gmail.com>
2021-08-17 17:20:34 +0000

    updated function

--- head.js
+++ head.js
@@ -1,15 +1,14 @@
-const assertEqual = function(actual, expected) {
-  console.assert(actual === expected);
-  console.assert(actual !== expected);
+const head = function(headArr) {
+  return headArr.splice(1);
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
 
-const head = function() {
-  _.head([]);
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+  console.assert(actual === expected);
+  console.assert(actual !== expected);
+  }
 };
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
-
