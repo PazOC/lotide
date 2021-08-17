8650cf2965dfa682991eb3996ceb7f0fd14195f6
Paz Ortega <paz.ortega.contreras@gmail.com>
2021-08-17 13:24:36 +0000

    added examples to assertEqual

--- assertEqual.js
+++ assertEqual.js
@@ -1,3 +1,18 @@
+const sum = function(a, b) {
+  return a + b;
+};
+
+console.assert(sum(1, 2) === 3);
+console.assert(sum(1, 20) === 3);
+
+const sumBuggy = function(a, b) {
+  return a * b;
+};
+
+console.assert(sumBuggy(1, 2) === 3);
+
+
+
 const assertEqual = function(actual, expected) {
   console.assert(actual === expected);
   console.assert(actual !== expected);
@@ -5,3 +20,9 @@ const assertEqual = function(actual, expected) {
 
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
+
+// const name = 'Alice';
+// console.log('Hello, ' + name + '!');
+
+const name = 'Alice';
+console.log(`Hello, ${name}!`);
