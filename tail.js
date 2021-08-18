08289b90aaba6d550ecd5e9adfa9f94eb0933059
Paz Ortega <paz.ortega.contreras@gmail.com>
2021-08-18 04:12:54 +0000

    improved tail function

--- tail.js
+++ tail.js
@@ -1,15 +1,5 @@
-const assertEqual = function(actual, expected) {
-  console.assert(actual === expected);
-  console.assert(actual !== expected);
+const tail = function() {
+  return array.slice(1);
 };
 
-const tail = function() {
-_.tail([1, ,2 ,3]);
-}
 
-const result = tail(['Hello', 'Lighthouse', 'Labs']);
-assertEqual(result, ['Lighthouse', 'Labs']);
-
-const words = ['Yo Yo', 'Lighthouse', 'Labs'];
-tail(words);
-assertEqual(words.length, 3);
