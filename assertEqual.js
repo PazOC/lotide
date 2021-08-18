9a9c7f7f94d05fe9e4bac0a2ee2d130fb365822d
Paz Ortega <paz.ortega.contreras@gmail.com>
2021-08-18 04:06:01 +0000

    improved funciton

--- assertEqual.js
+++ assertEqual.js
@@ -1,28 +1,11 @@
-const sum = function(a, b) {
-  return a + b;
-};
-
-console.assert(sum(1, 2) === 3);
-console.assert(sum(1, 20) === 3);
-
-const sumBuggy = function(a, b) {
-  return a * b;
-};
-
-console.assert(sumBuggy(1, 2) === 3);
-
-
-
 const assertEqual = function(actual, expected) {
-  console.assert(actual === expected);
-  console.assert(actual !== expected);
+  if (actual === expected) {
+    console.log(`✅  Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
+  }
 };
 
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 
-// const name = 'Alice';
-// console.log('Hello, ' + name + '!');
-
-const name = 'Alice';
-console.log(`Hello, ${name}!`);
