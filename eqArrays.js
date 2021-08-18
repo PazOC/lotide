89985f91fa842cd5076356f42d1fa3808e353798
Paz Ortega <paz.ortega.contreras@gmail.com>
2021-08-18 13:00:57 +0000

    updated formula

--- eqArrays.js
+++ eqArrays.js
@@ -23,3 +23,5 @@ assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+
+
