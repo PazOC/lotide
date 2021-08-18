9cf359f0f642635efcdea85b822f4a72a48c3c66
Paz Ortega <paz.ortega.contreras@gmail.com>
2021-08-18 04:11:35 +0000

    improved function

--- head.js
+++ head.js
@@ -1,14 +1,7 @@
 const head = function(headArr) {
-  return headArr.splice(1);
+    return array[0];
 };
 
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-  console.assert(actual === expected);
-  console.assert(actual !== expected);
-  }
-};
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
