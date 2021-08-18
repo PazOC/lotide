3e7363b4fd1e22b98b7315692a7a8761d3e48585
Paz Ortega <paz.ortega.contreras@gmail.com>
2021-08-18 22:18:59 +0000

    added countLetters function

---
+++ countLetters.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅  Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(string) {
+  const sentence = {};
+  for (const letter of string) {
+    if (letter !== ' ') {
+      if (letter in sentence) {
+        sentence[letter] += 1;
+      } else {
+        sentence[letter] = 1;
+      }
+    }
+  }
+  return sentence;
+
+};
+
+console.log(countLetters('Light House Labs'));
+
+
+
+
+
+
+
