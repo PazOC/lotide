92cf15d0deea232f041a51fc8ea49b716443f439
Paz Ortega <paz.ortega.contreras@gmail.com>
2021-08-19 00:29:49 +0000

    updated function

--- middle.js
+++ middle.js
@@ -36,7 +36,7 @@ console.log(middle([1])) // => []
 console.log(middle([1, 2])) // => []
 
 console.log(middle([1, 2, 3])) // => [2]
-console.log(middle([1, 2, 3, 4, 5]))// => [3]
+console.log(middle([1, 2, 3, 4, 5]))// => [3]   
 
 console.log(middle([1, 2, 3, 4, 5, 6])) // => [2, 3]
  // => [3, 4]
