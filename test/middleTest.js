316915a0235e4fa76e590cac1c1b14eee5a0a684
Paz Ortega <paz.ortega.contreras@gmail.com>
2021-08-25 02:18:59 +0000

    Added middle test file

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,18 @@
-const assertArraysEqual = require('../assertArraysEqual');
-const middle = require('../middle');
+// const assertArraysEqual = require('../assertArraysEqual');
+// const middle = require('../middle');
 
 
-console.log(assertArraysEqual(middle([1, 2]), ([])));
-console.log(assertArraysEqual(middle([1, 2, 3]), ([2])));
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]));
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]));
+// console.log(assertArraysEqual(middle([1, 2]), ([])));
+// console.log(assertArraysEqual(middle([1, 2, 3]), ([2])));
+// console.log(assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]));
+// console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]));
+
+
+const assert = require('chai').assert;
+const middle   = require('../middle');
+
+describe("#middle", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(middle([1, 2, 3]), 1);
+  });
+});
